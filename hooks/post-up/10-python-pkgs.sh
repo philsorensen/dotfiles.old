a9a2303daebbb62bcc1a0eeaa508f05a08fed5a8
Phillip Sorensen <phil.a.sorensen@gmail.com>
2019-07-02 13:06:52 -0400

    Add initial python3 setup

---
+++ hooks/post-up/10-python-pkgs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Install python packages and upgrade existing
+#
+
+needed_pkgs="pipenv"
+install_pkgs=""
+
+current_pkgs=$(pip3 list --user | awk 'NR>2 {print $1}' | tr '\n' ' ')
+
+for pkg in $needed_pkgs; do
+    if [[ "$current_pkgs" != *$pkg* ]]; then
+        install_pkgs="$install_pkgs $pkg"
+    fi
+done
+
+# update existing
+pip3 install -U --user $current_pkgs
+
+# install missing
+if [ -n $install_pkgs ]; then
+    pip3 install --user $install_pkgs
+fi
