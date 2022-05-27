ec736bd17948b40241e4100fc778c1323ef1aafe
Phillip Sorensen <phil.a.sorensen@gmail.com>
2022-05-26 21:30:07 -0400

    Add pip redirect code

--- hooks/post-up/10-python-pkgs.sh
+++ hooks/post-up/10-python-pkgs.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 #
-# Use pipx to install global python packages.
+# 1. Add pip-{version} links to ~/.local/bin/pip
+# 2. Install required packages and update installed packages with pipx.
 #
 
-# global packages 
-PKGS="pipenv"
+# create pip links
+major=$(python3 -c 'import sys; print(sys.version_info[0])')
+minor=$(python3 -c 'import sys; print(sys.version_info[1])')
 
+(cd ~/.local/bin; ln -s pip "pip${major}"; ln -s pip "pip${major}.${minor}")
+
+
+# required packages
+PKGS="pipenv"
 
 # install/update pipx
 python3 -m pip install --upgrade --user pipx
